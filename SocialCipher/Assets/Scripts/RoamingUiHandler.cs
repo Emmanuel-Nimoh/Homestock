6a495d056196267688644758978e5b5e6f764c0a
patel6nr <patel6nr@mail.uc.edu>
2025-01-01 15:25:37 -0500

    IntialCommit

---
+++ SocialCipher/Assets/Scripts/RoamingUiHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RoamingUiHandler : MonoBehaviour
+{
+    [SerializeField] TMP_Text hintTokenCount;
+
+    private void Start()
+    {
+        GameStateHandler.Instance.OnHintTokenUpdate += OnHintTokenUpdate;
+        OnHintTokenUpdate();
+    }
+
+    private void OnDestroy()
+    {
+        GameStateHandler.Instance.OnHintTokenUpdate -= OnHintTokenUpdate;
+    }
+
+    private void OnHintTokenUpdate()
+    {
+        hintTokenCount.text = GameStateHandler.Instance.GameData.HintTokenCount.ToString();
+    }
+}
